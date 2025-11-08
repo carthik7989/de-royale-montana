21037facc5cf4df441e1c64d662e7fab30d54c93
carthik7989 <cojective.karthik@gmail.com>
2025-11-08 18:55:08 +0530

    feat: expand fluid typography scale

--- src/app/typo/page.tsx
+++ src/app/typo/page.tsx
@@ -2,14 +2,41 @@ export default function Typo()
 {
     return (
         <div className="fp flex flex-col min-h-screen gap-y-6 bg-primary text-secondary">
+            <h1 className="f-h1-xl font-beautique">Heading 1 XL</h1>
             <h1 className="f-h1 font-beautique">Heading 1</h1>
+            <h1 className="f-h1-m font-beautique">Heading 1 M</h1>
+            <h1 className="f-h1-s font-beautique">Heading 1 S</h1>
+
+            <h2 className="f-h2-xl font-beautique">Heading 2 XL</h2>
             <h2 className="f-h2 font-beautique">Heading 2</h2>
+            <h2 className="f-h2-m font-beautique">Heading 2 M</h2>
+            <h2 className="f-h2-s font-beautique">Heading 2 S</h2>
+
+            <h3 className="f-h3-xl font-beautique">Heading 3 XL</h3>
             <h3 className="f-h3 font-beautique">Heading 3</h3>
+            <h3 className="f-h3-m font-beautique">Heading 3 M</h3>
+            <h3 className="f-h3-s font-beautique">Heading 3 S</h3>
+
+            <h4 className="f-h4-xl">Heading 4 XL</h4>
             <h4 className="f-h4">Heading 4</h4>
+            <h4 className="f-h4-m">Heading 4 M</h4>
+            <h4 className="f-h4-s">Heading 4 S</h4>
+
+            <h5 className="f-h5-xl">Heading 5 XL</h5>
             <h5 className="f-h5">Heading 5</h5>
+            <h5 className="f-h5-m">Heading 5 M</h5>
+            <h5 className="f-h5-s">Heading 5 S</h5>
+
+            <h6 className="f-h6-xl">Heading 6 XL</h6>
             <h6 className="f-h6">Heading 6</h6>
-            <p className="f-base">Paragraph</p>
-            <div className="f-xs">Paragraph</div>
+            <h6 className="f-h6-m">Heading 6 M</h6>
+            <h6 className="f-h6-s">Heading 6 S</h6>
+            <p className="f-base-xl">Paragraph XL</p>
+            <p className="f-base-l">Paragraph L</p>
+            <p className="f-base font-mona">Paragraph (default f-base)</p>
+            <p className="f-base-m">Paragraph M</p>
+            <p className="f-base-s">Paragraph S</p>
+            <p className="f-base-xs">Paragraph XS</p>
         </div>
     );
 }
